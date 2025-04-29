cde38bc85ff053449e901a4327baa4aece496e5f
Paulo Pereira <31625885+PJADPereira@users.noreply.github.com>
2025-04-29 23:32:11 +0100

    Create snape-pooled.sh

---
+++ snape-pooled.sh
@@ -0,0 +1,5 @@
+./software/snape-pooled/snape-pooled -nchr 64 -theta 0.001 -D 0.1 -fold folded -priortype informative < ./pileups/MON_AA_filtered.pileup 1> MON_AA_ng_SAB3.sfs 2> MON_AA_ng_SAB3.err
+./software/snape-pooled/snape-pooled -nchr 58 -theta 0.001 -D 0.1 -fold folded -priortype informative < ./pileups/BAG_AA_filtered.pileup 1> BAG_AA_ng_SAB1.sfs 2> BAG_AA_ng_SAB1.err
+./software/snape-pooled/snape-pooled -nchr 42 -theta 0.001 -D 0.1 -fold folded -priortype informative < ./pileups/CBD_AA_filtered.pileup 1> CBD_AA_ng_SAB2.sfs 2> CBD_AA_ng_SAB2.err
+./software/snape-pooled/snape-pooled -nchr 132 -theta 0.001 -D 0.1 -fold folded -priortype informative < ./pileups/ALQ_AA_filtered.pileup 1> ALQ_AA_ng__AD2.sfs 2> ALQ_AA_ng_AD2.err
+./software/snape-pooled/snape-pooled -nchr 96 -theta 0.001 -D 0.1 -fold folded -priortype informative < ./pileups/GAR_AA_filtered.pileup 1> GAR_AA_ng_AD5.sfs 2> GAR_AA_ng_AD5.err
