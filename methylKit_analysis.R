a1778ee8e18a7f53467ba0b0622e5057513e69a9
Paulo Pereira <31625885+PJADPereira@users.noreply.github.com>
2025-04-29 22:51:28 +0100

    Update methylKit_analysis.R

--- methylKit_analysis.R
+++ methylKit_analysis.R
@@ -1,9 +1,10 @@
+require(goseq)
 require(methylKit)
 require(ggplot2)
 require(genomation)
 require(dplyr)
-library(ggfortify)
-
+require(ggfortify)
+require(gplots)
 ## FUNCTION NECESSARY TO PREP THE FILES
 
 makeList <- function(vec) {
@@ -29,7 +30,6 @@ tej_files_list <- makeList(paste0(tej_files,list.files(tej_files,pattern = "bism
 cbd_files_list <- makeList(paste0(cbd_files,list.files(cbd_files,pattern = "bismark.cov.gz")))
 all_files <- c(tej_files_list,cbd_files_list)
 samples <- strsplit(c(list.files(tej_files,pattern = "bismark.cov.gz"),list.files(cbd_files,pattern = "bismark.cov.gz")),"_local_bed.gz.bismark.cov.gz")
-#condit <- c(rep(0,24),rep(1,24))
 condit <- c(rep(0,18),rep(1,19))
 indcover <- 3
 myobj <- methRead(location = all_files,
@@ -59,10 +59,11 @@ nsamples <- 10L
 
 
 ### LOAD THE COVARIATES - File covariate_Table.csv in github
-
 covariates <- read.table("covariate_Table.csv",sep=";",header= T)
 
 samples_to_order_covariates <-  as.vector(sapply(unlist(samples, use.names = FALSE),gsub,pattern="_local_bed.gz_filtered.bismark.cov.gz",replacement=""))
+
+
 to_keep_covs <- subset(covariates,covariates$Sample %in% samples_to_order_covariates)
 order_of_covs <- match(samples_to_order_covariates,to_keep_covs$Sample) 
 
@@ -90,7 +91,7 @@ data <- data.frame("sample"=meth@sample.ids,"treatment"=meth@treatment)
 data$population <- "CBD"
 data$population[which(data$treatment==0)] <- "TEJ"
 
-## since adding this later, to ensure code integrity create a new dataframe
+
 forplot_cov <- covariates
 names(forplot_cov) <- c("sample","sex","year","pop")
 
@@ -98,11 +99,9 @@ data <- merge(data,forplot_cov,by="sample")
 
 autoplot(pca, data = data, colour = 'population',shape="sex")+
   theme(
-    #axis.title.x=element_text("Genome position"),
     plot.margin=unit(c(0,0,0,0), "null"),panel.spacing=unit(c(0,0,0,0), "null"),
-    #axis.title.y=element_text("Coverage"),
     
-    panel.background=element_blank()#,panel.border=element_blank(),panel.grid.major=element_blank(),
+    panel.background=element_blank()
   )+
   theme(axis.line.x = element_line(color="black", size = .5),
         axis.line.y = element_line(color="black", size = .5),
@@ -140,9 +139,7 @@ dmc <- ggplot(diffmeth_cov)+
   geom_hline(yintercept=-25)+
   theme(axis.line=element_blank(),axis.text.x=element_blank(),
         axis.ticks=element_blank(),
-        #axis.title.x=element_text("Genome position"),
         plot.margin=unit(c(0,0,0,0), "null"),panel.spacing=unit(c(0,0,0,0), "null"),
-        #axis.title.y=element_text("Coverage"),
         legend.position="none",
         panel.background=element_blank(),panel.border=element_blank(),panel.grid.major=element_blank(),
         panel.grid.minor=element_blank(),plot.background=element_blank())
@@ -158,9 +155,7 @@ dmn <- ggplot(diffmeth_nor)+
   geom_hline(yintercept=-25)+
   theme(axis.line=element_blank(),axis.text.x=element_blank(),
         axis.ticks=element_blank(),
-        #axis.title.x=element_text("Genome position"),
         plot.margin=unit(c(0,0,0,0), "null"),panel.spacing=unit(c(0,0,0,0), "null"),
-        #axis.title.y=element_text("Coverage"),
         legend.position="none",
         panel.background=element_blank(),panel.border=element_blank(),panel.grid.major=element_blank(),
         panel.grid.minor=element_blank(),plot.background=element_blank())
@@ -176,10 +171,9 @@ myDiff25pcov=getMethylDiff(myDiff_cov,difference=diff,qvalue=qval)
 
 ### MEASURE WHAT TYPE OF FEATURES ARE DIFFERENTIALLY METHYLATED
 
+gene.obj <- readTranscriptFeatures("E:/OneDrive/Thesis/alosa_genome/ncbi_1.1/genomic_transcoder.bed12",remove.unusual=FALSE) # delete
 
-gene.obj <- readTranscriptFeatures("./ncbi_1.1/genomic_transcoder.bed12",remove.unusual=FALSE)
-#a <- annotateWithGeneParts(as(myDiff25p,"GRanges"),gene.obj)
-#a <- annotateWithFeatures(as(myDiff25p,"GRanges"),gff)
+#gene.obj <- readTranscriptFeatures("./ncbi_1.1/genomic_transcoder.bed12",remove.unusual=FALSE)
 
 diffAnn=annotateWithGeneParts(as(myDiff25pcov,"GRanges"),gene.obj)
 associations <- getAssociationWithTSS(diffAnn)
@@ -192,16 +186,162 @@ myDiff25ptable <- merge(myDiff25ptable,associations,by="linenumber")
 
 
 
+### PLOT THE HEATMAPS
+
+
+
+meth_data <- getData(meth)
+percentage_table <- meth_data[,c(1,2,3,4)]
+for (i in 0:((18+19)-1)){
+  start <- 5+(i*3)
+  stop <- 4+(i*3)+3
+  print(paste(5+(i*3),4+(i*3)+3))
+  temp <- meth_data[,c(start:stop)]
+  percentage_table <- cbind(percentage_table,(temp[,2]*100/temp[,1]))
+  
+}
+
+names(percentage_table) <- c(c("chr","start","end","strand"),getSampleID(meth))
+
+joined <- right_join(percentage_table, myDiff25ptable,by=c("chr","start","end","strand"))
+
+joined_matrix <- as.matrix(joined[,c(5:41)])
+
+lib_names <- c("TEJ-01", "TEJ-02", "TEJ-04", "TEJ-05", "TEJ-06", "TEJ-07", "TEJ-08", "TEJ-09", "TEJ-10", "TEJ-11", "TEJ-12", "TEJ-15", "TEJ-18", "TEJ-20", "TEJ-21", "TEJ-22", "TEJ-23", "TEJ-24", "CBD-02", "CBD-03", "CBD-04", "CBD-05", "CBD-07", "CBD-08", "CBD-09", "CBD-10", "CBD-11", "CBD-12", "CBD-13", "CBD-14", "CBD-16", "CBD-17", "CBD-18", "CBD-19", "CBD-20", "CBD-22", "CBD-24")
+
+colnames(joined_matrix) <- lib_names
+
+
+myColors <- colorRampPalette(c("#40ff00","#80ff00","#bfff00","#ffff00","#ffbf00","#ff8000","#ff4000"))(n=100)
+
+
+
+heatmap.2(joined_matrix, Colv = F, col = myColors, dendrogram="none",
+          trace = "none", density.info = "none",)
+
+
+
+nas <- is.na(joined_matrix)
+
+tej <- as.data.frame(joined_matrix[,c(1:18)])
+cbd <- as.data.frame(joined_matrix[,c(19:37)])
+
+apply(tej,1,sum)
+apply(cbd,1,sum)
+
+tej
+
+
+sub_tej <- setNames(data.frame(matrix(ncol=10,nrow=dim(joined)[1])),c(paste0("TEJ_",1:10)))
+sub_cbd <- setNames(data.frame(matrix(ncol=10,nrow=dim(joined)[1])),c(paste0("CBD_",1:10)))
+replicates <- 100
+
+for (i in 1:dim(joined)[1]){
+  d_mean <- 1
+  d_std <- .5
+  d_var <- 5
+  j <- 0
+  
+  repeat {
+    
+    
+    j <- j+1
+    
+    # take a sample from length
+    c_line <- tej[i,]
+    na_striped <- c_line[!is.na(c_line)]
+    f_mean <- mean(na_striped)
+    f_std <- sd(na_striped)
+    f_var <- var(na_striped)
+    
+    
+    sample_pop <- sample(na_striped, 10)
+    
+    s_mean <-  mean(sample_pop)
+    s_std <- sd(sample_pop)
+    s_var <- var(sample_pop)
+    
+    
+    # keep the sample when is is close enough
+    if(abs(f_mean - s_mean) < d_mean &
+       abs(f_std - s_std) < d_std &
+       abs(f_var - s_var) < d_var){
+      sub_tej[i,] <- sort(sample_pop)
+      break
+    }
+    
+    if (j%%10000){
+      d_mean <- d_mean+.5
+      d_std <- d_std+0.1
+      d_var <- d_var + 2
+    }
+  }
+  
+  
+}
+
+
+
+
+for (i in 1:dim(joined)[1]){
+  d_mean <- 1
+  d_std <- .5
+  d_var <- 5
+  j <- 0
+  
+  repeat {
+    
+    
+    j <- j+1
+    
+    # take a sample from length
+    c_line <- cbd[i,]
+    na_striped <- c_line[!is.na(c_line)]
+    f_mean <- mean(na_striped)
+    f_std <- sd(na_striped)
+    f_var <- var(na_striped)
+    
+    
+    sample_pop <- sample(na_striped, 10)
+    
+    s_mean <-  mean(sample_pop)
+    s_std <- sd(sample_pop)
+    s_var <- var(sample_pop)
+    
+    
+    # keep the sample when is is close enough
+    if(abs(f_mean - s_mean) < d_mean &
+       abs(f_std - s_std) < d_std &
+       abs(f_var - s_var) < d_var){
+      sub_cbd[i,] <- sort(sample_pop,decreasing=T)
+      break
+    }
+    
+    if (j%%1000){
+      d_mean <- d_mean+1
+      d_std <- d_std+0.5
+      d_var <- d_var + 5
+    }
+  }
+  
+  
+}
+
+#myColors <- colorRampPalette(c("#fff2cf","yellow","orange","red"))(n = 1000)
+myColors <- colorRampPalette(c("#40ff00","#80ff00","#bfff00","#ffff00","#ffbf00","#ff8000","#ff4000"))(n=100)
+mid <- rep(NA,dim(joined)[1]
+)
+all <- as.matrix(cbind(sub_tej,mid,sub_cbd))
+
+heatmap.2(all, Colv = F, col = myColors, dendrogram="none",
+          trace="none", density.info = "none")
 
 
 
 
 
+## GO ENRICHMENT ANALYSIS
 
-
-
-## go enrichment
-library(goseq)
 
 ##need to filter by to just keep the genes in the main chromossomes in case it adds to some aritficial imbalance when i diddnt allow some genes to be conseidred for differences in methylation
 setwd("/methyl_analysis/goanalysis/")
@@ -215,18 +355,18 @@ categories <- read.table("/methyl_analysis/goanalysis/STRINGS/src/goterms.csv")
 names(categories) <- c("name","category")
 
 enriched_genes <- unique(sapply(strsplit(associations$feature.name,";"),"[",3))
-#enriched_genes_promoters <- unique(sapply(strsplit(associationspromoter$feature.name,";"),"[",3))
+
 
 
 all_genes <- length_data$name
 
 goseq_vector = as.integer(all_genes %in% enriched_genes)
-#goseq_promoters_vector = as.integer(all_genes %in% enriched_genes_promoters)
+
 names(goseq_vector)<-all_genes
-#names(goseq_promoters_vector) <- all_genes
+
 
 pwf <- nullp(goseq_vector,bias.data=length_data$length)
-#pwf_promoter <- nullp(goseq_promoters_vector,bias.data=length_data$length)
+
 
 
 GO.wall = goseq(pwf,gene2cat=categories)
