f4d6b3cceb480c0607ba3080df2e23c62547b77f
Paulo Pereira <31625885+PJADPereira@users.noreply.github.com>
2025-05-28 19:18:15 +0100

    Update topGOanalysis.R

--- topGOanalysis.R
+++ topGOanalysis.R
@@ -1,31 +1,38 @@
-library(topGO)
-library(data.table)
-library(splitstackshape)
-##need to filter by to just keep the genes in the main chromossomes in case it adds to some aritficial imbalance when i diddnt allow some genes to be conseidred for differences in methylation
+library(goseq)
+
+
 setwd("./methyl_analysis/goanalysis/")
 
 
 length_data <- read.table("./STRINGS/src/gene_length.csv")
 names(length_data) <- c('name','length')
-all_genes <- length_data$name
 
-categories <- readMappings(file = "./categories_topGO.csv")
+categories <- read.table("./STRINGS/src/goterms.csv")
+names(categories) <- c("name","category")
 
-enriched_all <- read.table("./finalcov10xwcover10icover3nsamples12_annotated_all_formated.csv",header=TRUE)
-
-
-
+enriched_all <- read.table("./differentiallymethylatedregions.csv",header=TRUE,sep=";")
 enriched_genes <- unique(enriched_all$name)
 
-geneList <- factor(as.integer(all_genes %in% enriched_genes))
-names(geneList) <- all_genes
-
-GOdata <- new("topGOdata",ontology="BP",allGenes=geneList,annot=annFUN.gene2GO,gene2GO=categories)
-
-test.stat <- new("classicCount",testStatistic= GOFisherTest,name="Fisher test")
-resultFisher <- getSigGroups(GOdata,test.stat)
 
 
-allRes <- GenTable(GOdata, Fisher = resultFisher,topNodes=100)
-allRes$adjusted <- p.adjust(allRes$Fisher,method="BH")
-allRes
+
+all_genes <- length_data$name
+
+goseq_vector = as.integer(all_genes %in% enriched_genes)
+
+names(goseq_vector)<-all_genes
+
+pwf <- nullp(goseq_vector,bias.data=length_data$length)
+
+
+GO.wall = goseq(pwf,gene2cat=categories)
+GO.nobias=goseq(pwf,gene2cat=categories,method="Hypergeometric")
+plot(log10(GO.wall[,2]), log10(GO.nobias[match(GO.wall[,1],GO.nobias[,1]),2]), xlab="log10(Wallenius p-values)",ylab="log10(Sampling p-values)",xlim=c(-3,0))
+abline(0,1,col=3,lty=2)
+GO.nobias$adjusted <- p.adjust(GO.nobias$over_represented_pvalue, method="BH")
+
+
+# GO.nobias.adjusted <- GO.nobias
+# GO.nobias.adjusted$adjusted <- p.adjust(GO.nobias$over_represented_pvalue, method="BH")
+# enriched.GO=p.adjust(GO.nobias$over_represented_pvalue, method="BH")
+# #enriched.GO=GO.wall$category[GO.nobias$over_represented_pvalue<.05]
