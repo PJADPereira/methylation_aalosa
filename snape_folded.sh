0444ef61b4d1be08000c2518a85180e20ccd094d
Paulo Pereira <31625885+PJADPereira@users.noreply.github.com>
2025-05-28 21:04:47 +0100

    Create snape_folded.sh

---
+++ snape_folded.sh
@@ -0,0 +1,5 @@
+./snape-pooled/snape-pooled/snape-pooled -nchr 64 -theta 0.001 -D 0.1 -fold folded -priortype informative -spectrum < ./pileups/MON_AA_SAB3.pileup 1> MON_AA_SAB3.sfs 2> MON_AA_SAB3.err 
+./snape-pooled/snape-pooled/snape-pooled -nchr 58 -theta 0.001 -D 0.1 -fold folded -priortype informative -spectrum < ./pileups/BAG_AA_SAB1.pileup 1> BAG_AA_SAB1.sfs 2> BAG_AA_SAB1.err 
+./snape-pooled/snape-pooled/snape-pooled -nchr 42 -theta 0.001 -D 0.1 -fold folded -priortype informative -spectrum < ./pileups/CBD_AA_SAB2.pileup 1> CBD_AA_SAB2.sfs 2> CBD_AA_SAB2.err 
+./snape-pooled/snape-pooled/snape-pooled -nchr 132 -theta 0.001 -D 0.1 -fold folded -priortype informative -spectrum < ./pileups/ALQ_AA_AD2.pileup 1> ALQ_AA_AD2.sfs 2> ALQ_AA_AD2.err 
+./snape-pooled/snape-pooled/snape-pooled -nchr 96 -theta 0.001 -D 0.1 -fold folded -priortype informative -spectrum < ./pileups/GAR_AA_AD5.pileup 1> GAR_AA_AD5.sfs 2> GAR_AA_AD5.err 
